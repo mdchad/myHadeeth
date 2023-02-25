03b17ec69f922163c8a3b0454b2e1343699a835e
Aliy Akhbar <aliyakhbar@gmail.com>
2023-02-25 17:25:59 +0800

    Fixed compass direction

--- app/Content/Qibla.js
+++ app/Content/Qibla.js
@@ -6,73 +6,9 @@ import * as Sensors from 'expo-sensors';
 import Compass from '../components/compass';
 
 const Qiblat = () => {
-    const [compassHeading, setCompassHeading] = useState(0);
-    const [sensorData, setData] = useState({ x: 0, y: 0, z: 0 });
-    const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
-    const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
-        'loading...'
-    );
-
-    useEffect(() => {
-        CheckIfLocationEnabled();
-        GetCurrentLocation();
-    }, []);
-
-    const GetCurrentLocation = async () => {
-        let { status } = await Location.requestBackgroundPermissionsAsync();
-
-        if (status !== 'granted') {
-            Alert.alert(
-                'Permission not granted',
-                'Allow the app to use location service.',
-                [{ text: 'OK' }],
-                { cancelable: false }
-            );
-        }
-
-        let { coords } = await Location.getCurrentPositionAsync();
-
-        if (coords) {
-            const { latitude, longitude } = coords;
-            let response = await Location.reverseGeocodeAsync({
-                latitude,
-                longitude
-            });
-
-            for (let item of response) {
-                // change the address format as per your need
-                let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`;
-
-                setDisplayCurrentAddress(address);
-            }
-        }
-    };
-
-    const CheckIfLocationEnabled = async () => {
-        let enabled = await Location.hasServicesEnabledAsync();
-
-        if (!enabled) {
-            Alert.alert(
-                'Location Service not enabled',
-                'Please enable your location services to continue',
-                [{ text: 'OK' }],
-                { cancelable: false }
-            );
-        } else {
-            setLocationServiceEnabled(enabled);
-        }
-    };
-
-    const qiblat = {
-        latitude: 21.422487,
-        longitude: 39.826206
-    }
-
-    // compare qiblat with current location and rotate the compass
-
     return (
-        <SafeView className="flex-1 bg-white p-5 flex gap-3">
-            <Text>Qiblat</Text>
+        <SafeView className="flex-1 bg-white flex w-full h-full">
+            {/* <Text>Qiblat</Text>÷ */}
 
             <View className="flex items-center justify-center">
                 {/* <Text>{accelerometerData}</Text> */}
--- app/components/compass.js
+++ app/components/compass.js
@@ -1,186 +1,238 @@
-// import React, { useState, useEffect } from 'react';
-// import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import { Accelerometer } from 'expo-sensors';
-
-// export default function App({ compass }) {
-//     const [{ x, y, z }, setData] = useState({
-//         x: 0,
-//         y: 0,
-//         z: 0,
-//     });
-//     const [subscription, setSubscription] = useState(null);
-
-//     const _slow = () => Accelerometer.setUpdateInterval(1000);
-//     const _fast = () => Accelerometer.setUpdateInterval(16);
-
-//     const _subscribe = () => {
-//         setSubscription(
-//             Accelerometer.addListener(setData)
-//         );
-//     };
-
-//     const _unsubscribe = () => {
-//         subscription && subscription.remove();
-//         setSubscription(null);
-//     };
-
-//     useEffect(() => {
-//         _subscribe();
-//         return () => _unsubscribe();
-//     }, []);
-
-//     // find the compass heading
-//     const compassHeading = Math.atan2(y, x) * (180 / Math.PI) + 180;
-//     console.log(compassHeading);
-
-//     return (
-//         <View style={styles.container}>
-//             <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-//             <Text style={styles.text}>x: {x}</Text>
-//             <Text style={styles.text}>y: {y}</Text>
-//             <Text style={styles.text}>z: {z}</Text>
-
-// <Image
-//     className="w-64 h-64"
-//     style={[
-//         { transform: [{ rotate: `${360 - compassHeading}deg` }] },
-//     ]}
-//     resizeMode="contain"
-//     source={require('./../../assets/compass-qibla.png')}
-// />
-
-//             <View style={styles.buttonContainer}>
-//                 <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-//                     <Text>{subscription ? 'On' : 'Off'}</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
-//                     <Text>Slow</Text>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity onPress={_fast} style={styles.button}>
-//                     <Text>Fast</Text>
-//                 </TouchableOpacity>
-//             </View>
-//         </View>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//     }
-// });
-
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import * as Location from 'expo-location';
-import { Accelerometer } from 'expo-sensors';
+import { Image, View, Text, Dimensions, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
+import { Grid, Col, Row } from 'react-native-easy-grid';
+import { Magnetometer } from 'expo-sensors';
+import * as Haptics from 'expo-haptics';
+import ZeroDegree from './ZeroDegree--extracted-from-package.js';
 import { useSegments } from 'expo-router';
 
-const QiblaCompass = () => {
-    const [compassHeading, setCompassHeading] = useState(0);
-    const [qiblaDirection, setQiblaDirection] = useState(0);
-    const [phoneRotationDegree, setPhoneRotationDegree] = useState(0);
+const { height, width } = Dimensions.get('window');
+let _Mecca = { latitude: 21.42252, longitude: 39.82621 };
 
-    const route = useSegments();
+export default App = () => {
+    const [subscription, setSubscription] = useState(null);
+    const [magnetometer, setMagnetometer] = useState(0);
 
-    const qiblaLatitude = 21.422487;
-    const qiblaLongitude = 39.826206;
+    const [err, setError] = useState(null);
+    const [degree, setDegree] = useState(0);
+    const [log, setLog] = useState(null);
+
+    const routes = useSegments();
+
+    const _slow = () => Magnetometer.setUpdateInterval(1000);
+    const _fast = () => Magnetometer.setUpdateInterval(16);
 
     useEffect(() => {
-        Location.watchHeadingAsync((data) => {
-            setCompassHeading(data.trueHeading);
-        });
+        _toggle();
+        return () => {
+            _unsubscribe();
+        };
     }, []);
 
-    const [{ x, y, z }, setData] = useState({
-        x: 0,
-        y: 0,
-        z: 0,
-    });
-    const [subscription, setSubscription] = useState(null);
-
-    const _slow = () => Accelerometer.setUpdateInterval(1000);
-    const _fast = () => Accelerometer.setUpdateInterval(16);
+    const _toggle = () => {
+        if (subscription) {
+            _unsubscribe();
+        } else {
+            _subscribe();
+        }
+    };
 
     const _subscribe = () => {
-        if (route === 'Content' || route === 'Qiblat') {
-            console.log('subscribed');
-            
-            setSubscription(
-                Accelerometer.addListener((accelerometerData) => {
-                    const { x, y, z } = accelerometerData;
-                    const radians = Math.atan2(y, x);
-                    let degree = radians * (180 / Math.PI);
-                    if (degree < 0) {
-                        degree += 360;
-                    }
-                    setPhoneRotationDegree(degree);
-                    setData({ x, y, z });
-                })
-            );
-        }
+        setSubscription(
+            Magnetometer.addListener(result => {
+                setMagnetometer(_angle(result));
+            })
+        );
+
+        console.log('subscribed');
     };
 
     const _unsubscribe = () => {
         subscription && subscription.remove();
         setSubscription(null);
+
+        console.log('unsubscribed');
     };
 
+    const FILTER_SIZE = 5;
+
+    const _angle = (magnetometer) => {
+        let angle = 0;
+        if (magnetometer) {
+            let { x, y, z } = magnetometer;
+            if (Math.atan2(y, x) >= 0) {
+                angle = Math.atan2(y, x) * (180 / Math.PI);
+            } else {
+                angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+            }
+        }
+
+        // console.log('angle', angle);
+        return Math.round(smooth(angle));
+    };
+
+    const smooth = (() => {
+        const buffer = new Array(FILTER_SIZE).fill(0);
+        let index = 0;
+        let sum = 0;
+
+        return (value) => {
+            sum = sum - buffer[index] + value;
+            buffer[index] = value;
+            index = (index + 1) % FILTER_SIZE;
+            return sum / FILTER_SIZE;
+        };
+    })();
+
+    const _direction = (degree) => {
+        if (degree >= 22.5 && degree < 67.5) {
+            return 'NE';
+        }
+        else if (degree >= 67.5 && degree < 112.5) {
+            return 'E';
+        }
+        else if (degree >= 112.5 && degree < 157.5) {
+            return 'SE';
+        }
+        else if (degree >= 157.5 && degree < 202.5) {
+            return 'S';
+        }
+        else if (degree >= 202.5 && degree < 247.5) {
+            return 'SW';
+        }
+        else if (degree >= 247.5 && degree < 292.5) {
+            return 'W';
+        }
+        else if (degree >= 292.5 && degree < 337.5) {
+            return 'NW';
+        }
+        else {
+            return 'N';
+        }
+    };
+
+    // Match the device top with pointer 0° degree. (By default 0° starts from the right of the device.)
+    const _degree = (magnetometer) => {
+        return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+    };
+
+    // Getting qibla direction
     useEffect(() => {
-        _subscribe();
-        return () => _unsubscribe();
+        let zeroDegree;
+        async function initZeroDegree() {
+            zeroDegree = new ZeroDegree(_Mecca);
+            zeroDegree._getLogData = log => setLog(log);
+
+            await zeroDegree.watchAsync(
+                degree => {
+                    let rounded = Math.round(degree);
+                    let finalDegree = rounded < 0 ? rounded + 360 : rounded;
+                    setDegree(finalDegree);
+
+                    if (rounded === 0)
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                },
+                err => setError(err));
+        }
+
+        initZeroDegree();
+
+        return () => zeroDegree.unwatch();
     }, []);
 
     useEffect(() => {
-        async function fetchQiblaDirection() {
-            const { status } = await Location.requestForegroundPermissionsAsync();
-            if (status === 'granted') {
-                const location = await Location.getCurrentPositionAsync();
-                const { latitude, longitude } = location.coords;
-                const qiblaDirection = Math.atan2(
-                    qiblaLongitude - longitude,
-                    qiblaLatitude - latitude
-                ) * (180 / Math.PI);
-                setQiblaDirection(qiblaDirection);
-            }
+        // if routes has qibla in it, then it means we are in qibla screen
+        if (routes.includes('Qibla')) {
+            // subscribe to magnetometer
+            _subscribe();
+        } else {
+            // unsubscribe from magnetometer
+            _unsubscribe();
         }
-        fetchQiblaDirection();
-    }, [qiblaLatitude, qiblaLongitude]);
+    }, [routes]);
+
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Compass Heading: {compassHeading}°</Text>
-            <Image
-                source={require('./../../assets/compass-qibla.png')}
-                style={[
-                    styles.compass,
-                    {
-                        transform: [
-                            { rotate: `${360 - compassHeading}deg` },
-                            { rotateZ: `${phoneRotationDegree.toFixed(2)}deg` },
-                        ],
-                    },
-                ]}
-            />
-            <Text style={styles.text}>Qibla Direction: {qiblaDirection.toFixed(2)}°</Text>
-        </View>
+        <SafeAreaView className="w-full h-full ">
+            <Grid style={{ backgroundColor: 'black' }}>
+                <Row style={{ alignItems: 'center' }} size={.9}>
+                    <Col style={{ alignItems: 'center' }}>
+                        <Text
+                            style={{
+                                color: '#fff',
+                                fontSize: height / 26,
+                                fontWeight: 'bold'
+                            }}>
+                            {_direction(_degree(magnetometer))}
+                        </Text>
+                    </Col>
+                </Row>
+
+                <Row style={{ alignItems: 'center' }} size={.1}>
+                    <Col style={{ alignItems: 'center' }}>
+                        <View style={{ position: 'absolute', width: width, alignItems: 'center', top: -50 }}>
+                            <Image source={require('./../../assets/compass_pointer.png')} style={{
+                                height: height / 15,
+                                resizeMode: 'contain'
+                            }} />
+                        </View>
+                    </Col>
+                </Row>
+
+                <Row style={{ alignItems: 'center' }} size={2}>
+                    <Text style={{
+                        color: '#fff',
+                        fontSize: height / 27,
+                        width: width,
+                        position: 'absolute',
+                        textAlign: 'center'
+                    }}>
+                        {degree}°
+                    </Text>
+
+                    <Col style={{ alignItems: 'center' }}>
+
+                        <Image source={require("./../../assets/compass_bg.png")} style={{
+                            height: width,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            resizeMode: 'contain',
+                            transform: [{ rotate: 360 - magnetometer + 'deg' }]
+                        }} />
+
+                    </Col>
+                </Row>
+
+                <Row style={{ alignItems: 'center' }} size={1}>
+                    <Col style={{ alignItems: 'center' }}>
+                        <Text style={{ color: '#fff' }}>Magnetometer : {_degree(magnetometer)}°</Text>
+                    </Col>
+                    {/* <Col style={{ alignItems: 'center' }}>
+                        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+                            <Text>{subscription ? 'On' : 'Off'}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+                            <Text>Slow</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={_fast} style={styles.button}>
+                            <Text>Fast</Text>
+                        </TouchableOpacity>
+                    </Col> */}
+                </Row>
+            </Grid>
+        </SafeAreaView>
     );
-};
+}
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
+    button: {
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5
     },
-    text: {
-        fontSize: 20,
-        marginVertical: 10,
-    },
-    compass: {
-        width: 250,
-        height: 250,
-    },
+    middleButton: {
+        marginHorizontal: 0
+    }
 });
-
-export default QiblaCompass;
